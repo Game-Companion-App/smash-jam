1f28ad6c090256cc092e0510d9e8c9715a701a88
MikeRowManage <michael.priestley88@yahoo.com>
2020-04-07 16:22:57 -0600

    added stats seed file

---
+++ db/stats_seed.sql
@@ -0,0 +1,339 @@
+-- create table fighter_stats(
+--     stats_id serial primary key,
+--     weight int,
+--     walk_speed decimal,
+--     run_speed decimal,
+--     air_speed decimal,
+--     fall_speed decimal,
+--     fast_fall_speed decimal
+-- ) 
+
+-- mario
+-- insert into fighter_stats(weight, walk_speed, run_speed, air_speed, fall_speed, fast_fall_speed)
+-- values(98, 1.155, 1.76, 1.208, 1.5, 2.4);
+
+-- donkey kong
+-- insert into fighter_stats(weight, walk_speed, run_speed, air_speed, fall_speed, fast_fall_speed)
+-- values(127, 1.365, 1.873, 1.208, 1.63, 2.608);
+
+-- link
+-- insert into fighter_stats(weight, walk_speed, run_speed, air_speed, fall_speed, fast_fall_speed)
+-- values(104, 1.247, 1.534, 0.924, 1.6, 3.04);
+
+-- samus
+-- insert into fighter_stats(weight, walk_speed, run_speed, air_speed, fall_speed, fast_fall_speed)
+-- values(108, 1.115, 1.654, 1.103, 1.33, 2.128);
+
+-- dark samus
+-- insert into fighter_stats(weight, walk_speed, run_speed, air_speed, fall_speed, fast_fall_speed)
+-- values(108, 1.115, 1.654, 1.103, 1.33, 2.128);
+
+-- yoshi
+-- insert into fighter_stats(weight, walk_speed, run_speed, air_speed, fall_speed, fast_fall_speed)
+-- values(104, 1.208, 2.046, 1.344, 1.29, 2.064);
+
+-- kirby
+-- insert into fighter_stats(weight, walk_speed, run_speed, air_speed, fall_speed, fast_fall_speed)
+-- values(79, 0.977, 1.727, 0.84, 1.23, 1.968);
+
+-- fox
+-- insert into fighter_stats(weight, walk_speed, run_speed, air_speed, fall_speed, fast_fall_speed)
+-- values(77, 1.523, 2.402, 1.11, 2.1, 3.36);
+
+-- pikachu
+-- insert into fighter_stats(weight, walk_speed, run_speed, air_speed, fall_speed, fast_fall_speed)
+-- values(79, 1.302, 2.039, 0.957, 1.55, 2.48);
+
+-- luigi
+-- insert into fighter_stats(weight, walk_speed, run_speed, air_speed, fall_speed, fast_fall_speed)
+-- values(97, 1.134, 1.65, 0.77, 1.32, 2.112);
+
+-- ness
+-- insert into fighter_stats(weight, walk_speed, run_speed, air_speed, fall_speed, fast_fall_speed)
+-- values(94, 0.907, 1.609, 1.007, 1.31, 2.096);
+
+-- captain falcon
+-- insert into fighter_stats(weight, walk_speed, run_speed, air_speed, fall_speed, fast_fall_speed)
+-- values(104, 0.987, 2.552, 1.218, 1.865, 2.984);
+
+-- jigglypuff
+-- insert into fighter_stats(weight, walk_speed, run_speed, air_speed, fall_speed, fast_fall_speed)
+-- values(68, 0.735, 1.271, 1.332, 0.98, 1.568);
+
+-- peach
+-- insert into fighter_stats(weight, walk_speed, run_speed, air_speed, fall_speed, fast_fall_speed)
+-- values(89, 0.924, 1.595, 1.029, 1.19, 1.904);
+
+-- daisy
+-- insert into fighter_stats(weight, walk_speed, run_speed, air_speed, fall_speed, fast_fall_speed)
+-- values(89, 0.924, 1.595, 1.029, 1.19, 1.904);
+
+-- bowser
+-- insert into fighter_stats(weight, walk_speed, run_speed, air_speed, fall_speed, fast_fall_speed)
+-- values(135, 0.901, 1.971, 1.155, 1.77, 2.832);
+
+-- ice climbers
+-- insert into fighter_stats(weight, walk_speed, run_speed, air_speed, fall_speed, fast_fall_speed)
+-- values(92, 1.008, 1.53, 0.83, 1.3, 2.08);
+
+-- sheik
+-- insert into fighter_stats(weight, walk_speed, run_speed, air_speed, fall_speed, fast_fall_speed)
+-- values(78, 1.47, 2.42, 1.155, 1.75, 2.8);
+
+-- zelda
+-- insert into fighter_stats(weight, walk_speed, run_speed, air_speed, fall_speed, fast_fall_speed)
+-- values(85, 0.914, 1.43, 1.092, 1.35, 2.16);
+
+-- dr. mario
+-- insert into fighter_stats(weight, walk_speed, run_speed, air_speed, fall_speed, fast_fall_speed)
+-- values(98, 0.9173, 1.39779, 0.92388, 1.5, 2.4);
+
+-- pichu
+-- insert into fighter_stats(weight, walk_speed, run_speed, air_speed, fall_speed, fast_fall_speed)
+-- values(62, 1.302, 1.892, 1.029, 1.9, 2.5);
+
+-- falco
+-- insert into fighter_stats(weight, walk_speed, run_speed, air_speed, fall_speed, fast_fall_speed)
+-- values(82, 1.344, 1.619, 0.977, 1.8, 2.88);
+
+--marth
+-- insert into fighter_stats(weight, walk_speed, run_speed, air_speed, fall_speed, fast_fall_speed)
+-- values(90, 1.575, 1.964, 1.071, 1.58, 2.528);
+
+--lucina
+-- insert into fighter_stats(weight, walk_speed, run_speed, air_speed, fall_speed, fast_fall_speed)
+-- values(90, 1.575, 1.964, 1.071, 1.58, 2.528);
+
+--young link
+-- insert into fighter_stats(weight, walk_speed, run_speed, air_speed, fall_speed, fast_fall_speed)
+-- values(88, 1.26, 1.749, 0.966, 1.8, 2.88);
+
+--ganondorf
+-- insert into fighter_stats(weight, walk_speed, run_speed, air_speed, fall_speed, fast_fall_speed)
+-- values(118, 0.767, 1.34, 0.83, 1.65, 2.64);
+
+--mewtwo
+-- insert into fighter_stats(weight, walk_speed, run_speed, air_speed, fall_speed, fast_fall_speed)
+-- values(79, 1.26, 2.255, 1.313, 1.55, 2.48);
+
+--roy
+-- insert into fighter_stats(weight, walk_speed, run_speed, air_speed, fall_speed, fast_fall_speed)
+-- values(95, 1.208, 2.145, 1.302, 1.8, 2.88);
+
+--chrom
+-- insert into fighter_stats(weight, walk_speed, run_speed, air_speed, fall_speed, fast_fall_speed)
+-- values(95, 1.208, 2.145, 1.302, 1.8, 2.88);
+
+--mr. game and watch
+-- insert into fighter_stats(weight, walk_speed, run_speed, air_speed, fall_speed, fast_fall_speed)
+-- values(75, 1.18, 1.679, 1.176, 1.24, 1.984);
+
+--meta knight
+-- insert into fighter_stats(weight, walk_speed, run_speed, air_speed, fall_speed, fast_fall_speed)
+-- values(80, 1.239, 2.09, 1.04, 1.66, 2.656);
+
+--pit
+-- insert into fighter_stats(weight, walk_speed, run_speed, air_speed, fall_speed, fast_fall_speed)
+-- values(96, 1.259, 1.828, 0.935, 1.48, 2.368);
+
+--dark pit
+-- insert into fighter_stats(weight, walk_speed, run_speed, air_speed, fall_speed, fast_fall_speed)
+-- values(96, 1.259, 1.828, 0.935, 1.48, 2.368);
+
+--zero suit samus
+-- insert into fighter_stats(weight, walk_speed, run_speed, air_speed, fall_speed, fast_fall_speed)
+-- values(80, 1.47, 2.31, 1.26, 1.7, 2.72);
+
+--wario
+-- insert into fighter_stats(weight, walk_speed, run_speed, air_speed, fall_speed, fast_fall_speed)
+-- values(107, 0.889, 1.65, 1.271, 1.61, 2.576);
+
+--snake
+-- insert into fighter_stats(weight, walk_speed, run_speed, air_speed, fall_speed, fast_fall_speed)
+-- values(106, 0.882, 1.595, 0.987, 1.73, 2.768);
+
+--ike
+-- insert into fighter_stats(weight, walk_speed, run_speed, air_speed, fall_speed, fast_fall_speed)
+-- values(107, 0.912, 1.507, 1.134, 1.65, 2.64);
+
+--squirtle
+-- insert into fighter_stats(weight, walk_speed, run_speed, air_speed, fall_speed, fast_fall_speed)
+-- values(75, 1.281, 1.76, 1.01, 1.35, 2.16);
+
+--ivysaur
+-- insert into fighter_stats(weight, walk_speed, run_speed, air_speed, fall_speed, fast_fall_speed)
+-- values(96, 1.103, 1.595, 0.998, 1.38, 2.208);
+
+--charizard
+-- insert into fighter_stats(weight, walk_speed, run_speed, air_speed, fall_speed, fast_fall_speed)
+-- values(116, 1.187, 2.2, 1.103, 1.52, 2.432);
+
+--diddy kong
+-- insert into fighter_stats(weight, walk_speed, run_speed, air_speed, fall_speed, fast_fall_speed)
+-- values(90, 1.313, 2.006, 0.924, 1.75, 2.8);
+
+--lucas
+-- insert into fighter_stats(weight, walk_speed, run_speed, air_speed, fall_speed, fast_fall_speed)
+-- values(94, 0.893, 1.65, 1.55, 1.37, 2.192);
+
+--sonic
+-- insert into fighter_stats(weight, walk_speed, run_speed, air_speed, fall_speed, fast_fall_speed)
+-- values(86, 1.444, 3.85, 1.208, 1.65, 2.64);
+
+--king dedede
+-- insert into fighter_stats(weight, walk_speed, run_speed, air_speed, fall_speed, fast_fall_speed)
+-- values(127, 1.209, 1.496, 0.735, 1.95, 3.12);
+
+--olimar
+-- insert into fighter_stats(weight, walk_speed, run_speed, air_speed, fall_speed, fast_fall_speed)
+-- values(79, 0.945, 1.617, 0.861, 1.35, 2.16);
+
+--lucario
+-- insert into fighter_stats(weight, walk_speed, run_speed, air_speed, fall_speed, fast_fall_speed)
+-- values(92, 1.103, 1.705, 1.281, 1.68, 2.688);
+
+--r.o.b.
+-- insert into fighter_stats(weight, walk_speed, run_speed, air_speed, fall_speed, fast_fall_speed)
+-- values(106, 1.178, 1.725, 1.134, 1.6, 2.56);
+
+--toon link
+-- insert into fighter_stats(weight, walk_speed, run_speed, air_speed, fall_speed, fast_fall_speed)
+-- values(91, 1.288, 1.906, 1.05, 1.38, 2.208);
+
+--wolf
+-- insert into fighter_stats(weight, walk_speed, run_speed, air_speed, fall_speed, fast_fall_speed)
+-- values(92, 1.208, 1.54, 1.281, 1.8, 2.88);
+
+--villager
+-- insert into fighter_stats(weight, walk_speed, run_speed, air_speed, fall_speed, fast_fall_speed)
+-- values(92, 1.092, 1.397, 0.987, 1.32, 2.112);
+
+--mega man
+-- insert into fighter_stats(weight, walk_speed, run_speed, air_speed, fall_speed, fast_fall_speed)
+-- values(102, 1.132, 1.602, 1.155, 1.8, 2.88);
+
+--wii fit trainer
+-- insert into fighter_stats(weight, walk_speed, run_speed, air_speed, fall_speed, fast_fall_speed)
+-- values(96, 1.224, 1.866, 1.019, 1.3, 2.08);
+
+--rosalina and luma
+-- insert into fighter_stats(weight, walk_speed, run_speed, air_speed, fall_speed, fast_fall_speed)
+-- values(82, 1.138, 1.795, 1.05, 1.2, 1.92);
+
+--little mac
+-- insert into fighter_stats(weight, walk_speed, run_speed, air_speed, fall_speed, fast_fall_speed)
+-- values(87, 1.386, 2.464, 1.208, 1.95, 3.12);
+
+--greninja
+-- insert into fighter_stats(weight, walk_speed, run_speed, air_speed, fall_speed, fast_fall_speed)
+-- values(88, 1.502, 2.288, 1.239, 1.85, 2.96);
+
+--mii brawler
+-- insert into fighter_stats(weight, walk_speed, run_speed, air_speed, fall_speed, fast_fall_speed)
+-- values(94, 1.26, 1.92, 1.15, 1.92, 3.072);
+
+--mii swordfighter
+-- insert into fighter_stats(weight, walk_speed, run_speed, air_speed, fall_speed, fast_fall_speed)
+-- values(100, 1.07, 1.58, 1.17, 1.55, 2.48);
+
+--mii gunner
+-- insert into fighter_stats(weight, walk_speed, run_speed, air_speed, fall_speed, fast_fall_speed)
+-- values(104, 1.0, 1.37, 0.93, 1.45, 2.32);
+
+--palutena
+-- insert into fighter_stats(weight, walk_speed, run_speed, air_speed, fall_speed, fast_fall_speed)
+-- values(91, 1.271, 2.077, 1.0, 1.55, 2.48);
+
+--pac-man
+-- insert into fighter_stats(weight, walk_speed, run_speed, air_speed, fall_speed, fast_fall_speed)
+-- values(95, 1.097, 1.672, 1.092, 1.35, 2.16);
+
+--robin
+-- insert into fighter_stats(weight, walk_speed, run_speed, air_speed, fall_speed, fast_fall_speed)
+-- values(95, 0.936, 1.265, 1.05, 1.5, 2.4);
+
+--shulk
+-- insert into fighter_stats(weight, walk_speed, run_speed, air_speed, fall_speed, fast_fall_speed)
+-- values(97, 1.55, 1.672, 1.113, 1.58, 2.528);
+
+--bowser jr.
+-- insert into fighter_stats(weight, walk_speed, run_speed, air_speed, fall_speed, fast_fall_speed)
+-- values(108, 0.924, 1.566, 1.134, 1.65, 2.64);
+
+--duck hunt
+-- insert into fighter_stats(weight, walk_speed, run_speed, air_speed, fall_speed, fast_fall_speed)
+-- values(86, 1.213, 1.793, 1.155, 1.65, 2.64);
+
+--ryu
+-- insert into fighter_stats(weight, walk_speed, run_speed, air_speed, fall_speed, fast_fall_speed)
+-- values(103, 0.75, 1.6, 1.12, 1.6, 2.24);
+
+--ken
+-- insert into fighter_stats(weight, walk_speed, run_speed, air_speed, fall_speed, fast_fall_speed)
+-- values(103, 0.75, 1.6, 1.12, 1.6, 2.24);
+
+--cloud
+-- insert into fighter_stats(weight, walk_speed, run_speed, air_speed, fall_speed, fast_fall_speed)
+-- values(100, 1.155, 2.167, 1.155, 1.68, 2.688);
+
+--corrin
+-- insert into fighter_stats(weight, walk_speed, run_speed, air_speed, fall_speed, fast_fall_speed)
+-- values(98, 1.208, 1.595, 1.019, 1.65, 2.64);
+
+--bayonetta
+-- insert into fighter_stats(weight, walk_speed, run_speed, air_speed, fall_speed, fast_fall_speed)
+-- values(81, 0.945, 1.76, 1.019, 1.77, 2.832);
+
+--inkling
+-- insert into fighter_stats(weight, walk_speed, run_speed, air_speed, fall_speed, fast_fall_speed)
+-- values(94, 1.134, 1.925, 1.208, 1.58, 2.528);
+
+--ridley
+-- insert into fighter_stats(weight, walk_speed, run_speed, air_speed, fall_speed, fast_fall_speed)
+-- values(107, 1.1, 2.2, 1.05, 1.78, 2.848);
+
+--simon
+-- insert into fighter_stats(weight, walk_speed, run_speed, air_speed, fall_speed, fast_fall_speed)
+-- values(107, 0.76, 1.52, 0.94, 1.85, 2.96);
+
+--richter
+-- insert into fighter_stats(weight, walk_speed, run_speed, air_speed, fall_speed, fast_fall_speed)
+-- values(107, 0.76, 1.52, 0.94, 1.85, 2.96);
+
+--king k. rool
+-- insert into fighter_stats(weight, walk_speed, run_speed, air_speed, fall_speed, fast_fall_speed)
+-- values(133, 0.903, 1.485, 0.945, 1.7, 2.72);
+
+--isabelle
+-- insert into fighter_stats(weight, walk_speed, run_speed, air_speed, fall_speed, fast_fall_speed)
+-- values(88, 1.14, 1.48, 1.02, 1.3, 2.08);
+
+--incineroar
+-- insert into fighter_stats(weight, walk_speed, run_speed, air_speed, fall_speed, fast_fall_speed)
+-- values(116, 0.62, 1.18, 0.88, 1.76, 2.816);
+
+--piranha plant
+-- insert into fighter_stats(weight, walk_speed, run_speed, air_speed, fall_speed, fast_fall_speed)
+-- values(112, 0.76, 1.72, 1.0, 1.95, 2.73);
+
+--joker
+-- insert into fighter_stats(weight, walk_speed, run_speed, air_speed, fall_speed, fast_fall_speed)
+-- values(93, 1.12, 2.06, 1.1, 1.63, 3.097);
+
+--hero
+-- insert into fighter_stats(weight, walk_speed, run_speed, air_speed, fall_speed, fast_fall_speed)
+-- values(101, 0.98, 1.84, 1.01, 1.57, 2.512);
+
+--banjo and kazooie
+-- insert into fighter_stats(weight, walk_speed, run_speed, air_speed, fall_speed, fast_fall_speed)
+-- values(106, 1.06, 2.18, 0.92, 1.76, 2.816);
+
+--terry
+-- insert into fighter_stats(weight, walk_speed, run_speed, air_speed, fall_speed, fast_fall_speed)
+-- values(108, 0.85, 1.72, 0.95, 1.48, 2.368);
+
+--byleth
+-- insert into fighter_stats(weight, walk_speed, run_speed, air_speed, fall_speed, fast_fall_speed)
+-- values(97, 0.97, 1.43, 0.89, 1.6, 2.56);
+
+-- select * from fighter_stats;
