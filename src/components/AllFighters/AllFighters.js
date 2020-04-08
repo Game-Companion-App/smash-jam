2f07cd671d4ddefc7fd8e0a5da28f36a04d58d74
Anders Lofgran <anders.lofgran@gmail.com>
2020-04-08 15:48:58 -0600

    updated sidebar & added some routes

---
+++ src/components/AllFighters/AllFighters.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function AllFighters(props) {
+  return (
+    <div>
+      AllFighters
+    </div>
+  );
+}
+
+export default AllFighters;
---
+++ src/components/Tournament/TournamentBracket.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function TournamentBracket(props) {
+  return (
+    <div>
+      TournamentBracket
+    </div>
+  );
+}
+
+export default TournamentBracket;
