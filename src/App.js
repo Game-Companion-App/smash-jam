1a6ec775e135dbd6592bef302d6f7f77b98653b5
Joshua Smith <joshuasmith@Joshs-iMac.local>
2020-04-10 12:06:37 -0600

    resolved conflicts

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import Home from './components/Home/Home';
 import Profile from './components/Profile/Profile';
+import routes from './routes';
+import { withRouter } from 'react-router-dom';
 import './App.css';
 
 function App() {
 	return (
 		<div className="app">
 			<Home />
+			{routes}
 			<Profile />
 		</div>
 	);
 }
 
-export default App;
+export default withRouter(App);
