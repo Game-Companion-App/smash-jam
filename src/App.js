90e7d948b7aa8eb1a7052e619a828a433dce81e3
Joshua Smith <joshuasmith@Joshs-iMac.local>
2020-04-10 12:10:16 -0600

    updating

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import Home from './components/Home/Home';
 import Profile from './components/Profile/Profile';
 import routes from './routes';
 import { withRouter } from 'react-router-dom';
@@ -8,7 +7,6 @@ import './App.css';
 function App() {
 	return (
 		<div className="app">
-			<Home />
 			{routes}
 			<Profile />
 		</div>
