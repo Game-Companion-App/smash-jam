859dcad1cc6dfcda8521be0d52416a47ae7d4f90
Joshua Smith <joshuasmith@Joshs-iMac.local>
2020-04-10 23:10:52 -0600

    pulling master

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,16 +1,26 @@
 import React from 'react';
+// import News from "../News/News"
 import './Home.css';
 
 export default function Home() {
 	return (
-		<div>
+		<div className="home-container">
 			<div className="slideshow">
 				<div className="images" />
-				<img src="https://smash-app.s3-us-west-1.amazonaws.com/panoramic.png" />
+				<img src="https://smash-app.s3-us-west-1.amazonaws.com/panoramic.png" alt=""/>
 			</div>
 			<div className="nav-section">
-				<img className="logo" src="https://smash-app.s3-us-west-1.amazonaws.com/Smash+Bros+Logo.png" />
+				<img className="logo" src="https://smash-app.s3-us-west-1.amazonaws.com/Smash+Bros+Logo.png" alt="" />
+        <div className="nav">
+          <button className="nav-button">
+            Fighters
+          </button>
+          <button className="nav-button">
+            Tournaments
+          </button>
+        </div>
 			</div>
+      {/* <News /> */}
 		</div>
 	);
 }
