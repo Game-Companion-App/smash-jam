f311e8eff4298278b4ac5622b3098e075b3f9cdb
Joshua Smith <joshuasmith@Joshs-iMac.local>
2020-04-15 17:13:33 -0600

    character pages completed except for video link

--- server/controllers/fighterController.js
+++ server/controllers/fighterController.js
@@ -17,7 +17,15 @@ module.exports = {
     let fighter = await db.get_fighter_id([req.params.id]);
     console.log(fighter);
     if (fighter[0]) {
-      res.status(200).send(fighter);
+      let strong1 = await db.get_fighter_id([fighter[0].strong_1]);
+      let strong2 = await db.get_fighter_id([fighter[0].strong_2]);
+      let strong3 = await db.get_fighter_id([fighter[0].strong_3]);
+      let weak1 = await db.get_fighter_id([fighter[0].weak_1]);
+      let weak2 = await db.get_fighter_id([fighter[0].weak_2]);
+      let weak3 = await db.get_fighter_id([fighter[0].weak_3]);
+      res
+        .status(200)
+        .send({ fighter, strong1, strong2, strong3, weak1, weak2, weak3 });
     } else {
       res.status(500).send({ message: "Failed to retrieve fighter" });
     }
