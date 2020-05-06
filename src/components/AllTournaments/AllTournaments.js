968bc3391f884f9ab390d0c088648781bc0e8b9e
Anders Lofgran <anders.lofgran@gmail.com>
2020-05-06 14:54:37 -0600

    you can press enter to join/host tournaments insted of having to use all those muscles to move the mouse down a bit, josh

--- src/components/AllTournaments/AllTournaments.js
+++ src/components/AllTournaments/AllTournaments.js
@@ -38,9 +38,21 @@ function AllTournaments(props) {
     return (
       <div key={i}>
         <div> Tournament Name: {tournament.tournament_name} </div>
-        <div> Password: <input type='password' onChange={ev => setPassword(ev.target.value)}/> </div>
+        <div>
+          Password:
+          <input  type='password'
+                  onChange={ev => setPassword(ev.target.value)}
+                  onKeyUp={ev => {
+                    if(ev.keyCode === 13){
+                      if(password !== tournament.tournament_password){
+                        alert('Incorrect password')
+                        ev.preventDefault()
+                      }
+                      else props.history.push(`/tournament/${tournament.tournament_key}`)
+                    } else return
+                  }}/>
+        </div>
         <Link onClick={ev => {
-                if(!password) ev.preventDefault()
                 if(password !== tournament.tournament_password){
                   alert('Incorrect password')
                   ev.preventDefault()
@@ -80,7 +92,24 @@ function AllTournaments(props) {
       <div>
         <h4>Host Tournament</h4>
         <div> Tournament Name: <input type='text' onChange={ev => setTournamentName(ev.target.value)}/> </div>
-        <div> Password: <input type='password' onChange={ev => setPassword(ev.target.value)}/> </div>
+        <div>
+          Password:
+          <input type='password'
+            onChange={ev => setPassword(ev.target.value)}
+            onKeyUp={ev => {
+              if(ev.keyCode === 13){
+                if(!tournamentName || !password){
+                  alert('Please complete all fields')
+                  ev.preventDefault()
+                }
+                setTournamentName('')
+                setPassword('')
+                setBracketSize(8)
+                createTournament()
+              }
+            }}
+            />
+        </div>
         <div> 
           Bracket Size:
           <select type='text' onClick={ev => handleBracketSize(ev.target.value)} defaultValue={8}>
